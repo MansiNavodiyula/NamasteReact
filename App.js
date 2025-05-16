f716725e32064a5378d98d5db93b51e9726c4ffa
MansiNavodiyula <mansinavodiyula07@gmail.com>
2025-05-16 21:26:40 +0530

    TILL episode 3

--- App.js
+++ App.js
@@ -1,6 +1,29 @@
-// const txt=React.createElement('h1',{id:'txt'},'The World is urs..');
-const root=ReactDOM.createRoot(document.getElementById('root'));
-const parent =React.createElement('div',{id:"parent"},
-    React.createElement('div',{id:"Child"},
-        [React.createElement('h1',{},"Heyoo , am H1"),React.createElement('h2',{},"Heyyoo , am h2")]));
-root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//Creating element using core React
+// const txt=React.createElement("h1",{id:"h1tag"},"its raining");
+//Creating element using JSX (html/xml -like syntax )- not html in js
+// JSX (transpiled before it reaches it JS) - Parcel - Babel
+// const jsxEle=<h1 id="h1JSX">Heading using jsx</h1>
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+//root.render(txt);
+// root.render(jsxEle);
+
+//Ep-3
+const elem=<span>Woahhh</span>
+const Title=()=>(
+    <h2>7777
+        
+        {elem}
+    </h2>
+);
+const HeadingComp=()=>(
+        <div id="container">
+            <h1>React Comp</h1>
+            <Title/>  //Component Composition
+            <h3>{77+100}</h3>
+        </div>
+);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComp/>);
